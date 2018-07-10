b7e9e5a8d83468553bfe927cfd4db5801e5681c7
Oluwapelumi Egunjobi <pelumitegunjobi@gmail.com>
2018-07-10 13:56:41 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,3 +1,33 @@
+guest_name = Annabeth
+
+party_name = Camp Half-Blood Reunion Party
+
+date = July 10 
+
+time = 6:30 pm
+
+host_name = Percy 
+
+puts " Dear #{guest_name}"
+
+puts "You are cordially invited to the #{party_name} on #{date} at #{time}. Please RSVP no later than July 1st. "
+
+puts "Sincerly,"
+
+
+puts "Harry Potter"
+
+
+
+
+
+
+
+
+
+
+
+
 # Code your prompts here!
 
 # Try starting out with puts'ing a string.
